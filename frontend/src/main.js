bd2335c857e440c2996a039ec5976967e587df17
Bomsiwakorn <bomsiwakorns@gmail.com>
2020-07-09 13:51:39 +0700

    Install bootstrap4

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import * as jquery from 'jquery'
+
+
 Vue.config.productionTip = false
+Vue.prototype.jquery = jquery
 
 new Vue({
   render: h => h(App),
