bd1f87cd4d19352fc6a40fde604c0d94dc415b81
Bomsiwakorn <bomsiwakorns@gmail.com>
2020-07-13 09:37:37 +0700

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 5000;
+const PORT = 3000;
 
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: "./files",
+  destination: "./uploads",
   filename(req, file, cb) {
     cb(null, `${new Date()}-${file.originalname}`);
   },
@@ -14,19 +14,20 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-const config = {
-  headers: "Content-Type : multipart/form-data",
-};
-app.use(cors(config));
+// const config = {
+//   headers: "Content-Type : multipart/form-data"
+// };
+app.use(cors());
 
 app.post("/upload", upload.single("file"), (req, res) => {
-  req.header("Content-Type : multipart/form-data");
+  // req.header("Content-Type : multipart/form-data");
   const data = req.body;
   const file = req.file;
-  console.log(file.originalname);
+  console.log(file);
+  console.log(data);
   res.send({ file: file, data: data });
 });
 
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
